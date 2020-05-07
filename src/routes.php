8d33ed439320f29e9cfe48d6f2b39e01f3cac5d4
baclv <baclv@maxgroup.vn>
2020-05-07 12:40:50 +0700

    modify src/routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,2 +1,9 @@
 <?php
 Route::get('sample-admin-users', 'BacLV\Simple\SimpleController@index');
+Route::get('hello', function(){
+    echo 'Hello from the calculator package!';
+});
+Route::get('test-auth', function(){
+    echo 'Kiểm tra đăng nhập!';
+})->middleware('auth');
+
