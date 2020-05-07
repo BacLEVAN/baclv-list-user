2ce61fcbc1c70aaee60a21330d4c7356576ce563
baclv <baclv@maxgroup.vn>
2020-05-07 16:23:40 +0700

    modify src/views/admin.blade.php

--- src/views/admin.blade.php
+++ src/views/admin.blade.php
@@ -1,10 +1,35 @@
+@extends('layouts.app')
+@section('content')
 <div class="container">
-    <h1>You have {{count($users)}} user(s)</h1>
+    <h3 class="text-center">You have {{count($users)}} user(s)</h3>
     <div class="col-md-8">
-        <ul class="list-group">
-            @foreach($users as $user)
-                <li class="list-group-item">{{$user->id . ' | ' . $user->name}}</li>
-            @endforeach
-        </ul>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Login type</th>
+                    <th>Address</th>
+                    <th>Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->login_type}}</td>
+                        <td>{{$user->address}}</td>
+                        <td>{{$user->status}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+
     </div>
 </div>
+@stop
